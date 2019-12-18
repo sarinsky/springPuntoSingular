014717761f1f93bd5b51fc346c117958990dc9de
sarinsky <brownie_cloud5@outlook.com>
2019-12-18 17:15:33 -0600

    segundo commit

---
+++ src/main/java/com/example/demo/PuntoSingularApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntoSingularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntoSingularApplication.class, args);
+	}
+
+}
